98b77f3de47c06b807511b7545087d6db9ed2c3c
xMasta1243x <rodrigoyessiz@gmail.com>
2025-07-26 17:55:41 -0400

    Arma iniciacion
    Probando el tipo de arma y haciendo el codigo para tomar el arma

---
+++ Assets/Scripts/Interactable/InteractableSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractableSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string promptMessage;
+
+    public void BaseInteract()
+    {
+        Interact();
+    }
+    protected virtual void Interact()
+    {
+
+    }
+}
